33d20a1ae42784d6413c5d45ada50adc09847f23
AlexHaxe <Alexander.Blum@gmail.com>
2019-06-27 22:34:19 +0200

    first Travis run

--- benchmark.sh
+++ benchmark.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-readonly SRC_FOLDER=../src
+readonly DATA=$*
 
 # preformat SRC_FOLDER
-./out/cpp/Cli -s $SRC_FOLDER > /dev/null 2>&1
+./out/cpp/Cli $DATA > /dev/null 2>&1
 
-./out/cpp/Cli -s $SRC_FOLDER
-node out/formatter.js -s $SRC_FOLDER
-java -jar out/java/Cli.jar -s $SRC_FOLDER
-java -jar out/jvm/Cli.jar -s $SRC_FOLDER
-hl out/formatter.hl -s $SRC_FOLDER
-neko out/formatter.n -s $SRC_FOLDER
-php out/php/index.php -s $SRC_FOLDER
-python3 out/formatter.py -s $SRC_FOLDER
-mono out/cs/bin/Cli.exe -s $SRC_FOLDER
-lua out/formatter.lua -s $SRC_FOLDER
+./out/cpp/Cli $DATA
+node out/formatter.js $DATA
+java -jar out/java/Cli.jar $DATA
+java -jar out/jvm/Cli.jar $DATA
+hl out/formatter.hl $DATA
+neko out/formatter.n $DATA
+php out/php/index.php $DATA
+python3 out/formatter.py $DATA
+mono out/cs/bin/Cli.exe $DATA
+lua out/formatter.lua $DATA
