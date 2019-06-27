12c1343c5ad6f89f654a70c195a4bf0742ca7f60
AlexHaxe <Alexander.Blum@gmail.com>
2019-06-27 22:48:34 +0200

    install hxcpp and hashlink

--- benchmark.sh
+++ benchmark.sh
@@ -9,7 +9,7 @@ readonly DATA=$*
 node out/formatter.js $DATA
 java -jar out/java/Cli.jar $DATA
 java -jar out/jvm/Cli.jar $DATA
-hl out/formatter.hl $DATA
+$HL_BINARY out/formatter.hl $DATA
 neko out/formatter.n $DATA
 php out/php/index.php $DATA
 python3 out/formatter.py $DATA
