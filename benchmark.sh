e19f8bfdc6d4cca7c6c7a0614c248011863ff878
AlexHaxe <Alexander.Blum@gmail.com>
2019-06-28 01:43:23 +0200

    reset test repos after each run

--- benchmark.sh
+++ benchmark.sh
@@ -1,17 +1,27 @@
-#!/bin/bash
+#!/bin/bash -x
 
 readonly DATA=$*
 
-# preformat SRC_FOLDER
-./out/cpp/Cli $DATA
+function resetGit {
+    for i in $( ls data ) ; do (cd data/$i; git checkout -- .); done;
+}
 
 ./out/cpp/Cli $DATA
+resetGit
 node out/formatter.js $DATA
+resetGit
 java -jar out/java/Cli.jar $DATA
+resetGit
 java -jar out/jvm/Cli.jar $DATA
+resetGit
 # $HL_BINARY out/formatter.hl $DATA
+# resetGit
 neko out/formatter.n $DATA
+resetGit
 # php out/php/index.php $DATA
+# resetGit
 python3 out/formatter.py $DATA
+# resetGit
 # mono out/cs/bin/Cli.exe $DATA
+# resetGit
 # lua out/formatter.lua $DATA
