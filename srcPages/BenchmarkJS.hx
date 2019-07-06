165c5dc71588647049090aae32b0d26efcf009e8
AlexHaxe <Alexander.Blum@gmail.com>
2019-07-07 00:32:58 +0200

    show some graphs

--- srcPages/BenchmarkJS.hx
+++ srcPages/BenchmarkJS.hx
@@ -1,5 +1,8 @@
 import haxe.Http;
 import js.Browser;
+import js.Syntax;
+import js.html.CanvasRenderingContext2D;
+import js.html.CanvasElement;
 import js.jquery.JQuery;
 import json2object.JsonParser;
 import data.TestRun;
@@ -32,7 +35,7 @@ class BenchmarkJS {
 			haxe3Data = parser.fromJson(data, "archiveHaxe3.json");
 			checkLoaded();
 		}
-		request.onData = function(msg:String) {
+		request.onError = function(msg:String) {
 			trace("failed to download Haxe 3 data: " + msg);
 		}
 		request.request();
@@ -44,7 +47,7 @@ class BenchmarkJS {
 			haxe4Data = parser.fromJson(data, "archiveHaxe4.json");
 			checkLoaded();
 		}
-		request.onData = function(msg:String) {
+		request.onError = function(msg:String) {
 			trace("failed to download Haxe 4 data: " + msg);
 		}
 		request.request();
@@ -64,8 +67,164 @@ class BenchmarkJS {
 	}
 
 	function showData() {
-		trace("showData");
-		trace(haxe3Data);
-		trace(haxe4Data);
+		showLatest();
+		showHistory(Cpp, "cppBenchmarks");
+		showHistory(Java, "javaBenchmarks");
+		showHistory(Jvm, "jvmBenchmarks");
+		showHistory(Hashlink, "hlBenchmarks");
+		showHistory(NodeJs, "nodeBenchmarks");
+		showHistory(Neko, "nekoBenchmarks");
+		showHistory(Php, "phpBenchmarks");
+		showHistory(Python, "pythonBenchmarks");
 	}
+
+	function showLatest() {
+		var latestHaxe3Data:TestRun = haxe3Data[haxe3Data.length - 1];
+		var latestHaxe4Data:TestRun = haxe4Data[haxe4Data.length - 1];
+		var labels:Array<String> = [Cpp, Cs, Hashlink, Java, Jvm, Neko, NodeJs, Php, Python];
+
+		var haxe3Dataset = {
+			label: latestHaxe3Data.haxeVersion,
+			backgroundColor: "#FF6666",
+			borderColor: "#FF0000",
+			borderWidth: 1,
+			data: [for (label in labels) null]
+		};
+
+		var haxe4Dataset = {
+			label: latestHaxe4Data.haxeVersion,
+			backgroundColor: "#6666FF",
+			borderColor: "#0000FF",
+			borderWidth: 1,
+			data: [for (label in labels) null]
+		};
+
+		var data = {
+			labels: labels,
+			datasets: [haxe3Dataset, haxe4Dataset]
+		};
+		for (target in latestHaxe3Data.targets) {
+			var index:Int = data.labels.indexOf(target.name);
+			if (index < 0) {
+				continue;
+			}
+			haxe3Dataset.data[index] = Math.round(target.time * 1000) / 1000;
+		}
+		for (target in latestHaxe4Data.targets) {
+			var index:Int = data.labels.indexOf(target.name);
+			if (index < 0) {
+				continue;
+			}
+			haxe4Dataset.data[index] = Math.round(target.time * 1000) / 1000;
+		}
+		var ctx:CanvasRenderingContext2D = cast(Browser.document.getElementById("latestBenchmarks"), CanvasElement).getContext("2d");
+
+		var options = {
+			type: "bar",
+			data: data,
+			options: {
+				responsive: true,
+				legend: {
+					position: "top",
+				},
+				title: {
+					display: true,
+					text: "latest benchmark results"
+				},
+				tooltips: {
+					mode: "index",
+					intersect: false
+				},
+				hover: {
+					mode: "nearest",
+					intersect: true
+				},
+				scales: {
+					yAxes: [
+						{
+							scaleLabel: {
+								display: true,
+								labelString: "runtime in seconds"
+							}
+						}
+					]
+				}
+			}
+		};
+		Syntax.code("new Chart ({0}, {1})", ctx, options);
+	}
+
+	function showHistory(target:Target, canvasId:String) {
+		var haxe4Dataset = {
+			label: target,
+			backgroundColor: "#6666FF",
+			borderColor: "#0000FF",
+			borderWidth: 1,
+			fill: false,
+			data: []
+		};
+		var data = {
+			labels: [],
+			datasets: [haxe4Dataset]
+		};
+
+		for (run in haxe4Data) {
+			var time:Null<Float> = getHistoryTime(run, target);
+			if (time == null) {
+				continue;
+			}
+			data.labels.push(run.date);
+			haxe4Dataset.data.push(Math.round(time * 1000) / 1000);
+		}
+
+		var ctx:CanvasRenderingContext2D = cast(Browser.document.getElementById(canvasId), CanvasElement).getContext("2d");
+
+		var options = {
+			type: "line",
+			data: data,
+			options: {
+				responsive: true,
+				legend: {
+					position: "top",
+				},
+				title: {
+					display: true,
+					text: '$target benchmark results'
+				},
+				scales: {
+					yAxes: [
+						{
+							scaleLabel: {
+								display: true,
+								labelString: "runtime in seconds"
+							}
+						}
+					]
+				}
+			}
+		};
+		Syntax.code("new Chart ({0}, {1})", ctx, options);
+	}
+
+	function getHistoryTime(testRun:TestRun, target:Target):Null<Float> {
+		for (runTarget in testRun.targets) {
+			if (target == runTarget.name) {
+				return runTarget.time;
+			}
+		}
+		return null;
+	}
+}
+
+@:enum
+abstract Target(String) to String {
+	var Cpp = "C++";
+	var Cs = "C#";
+	var Hashlink = "Hashlink";
+	var Java = "Java";
+	var Jvm = "JVM";
+	var Neko = "Neko";
+	var NodeJs = "NodeJS";
+	var Php = "PHP";
+	var Python = "Python";
 }
