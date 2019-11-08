f9fef084d287bd4e5e225f205624a1dbcd3e3ad0
AlexHaxe <Alexander.Blum@gmail.com>
2019-11-08 01:21:25 +0100

    changed to Haxelib versions of formatter and tokentree
    added CSV a more generic onverter
    disabled PHP7 run for Haxe 3.4.7 since it hangs with git lime sources

--- prepare.sh
+++ prepare.sh
@@ -4,10 +4,6 @@ cd "$( dirname "${BASH_SOURCE[0]}" )"
 git checkout -- .
 git pull
 
-git clone https://github.com/HaxeCheckstyle/haxe-formatter.git --depth 1
-cp haxe-formatter/haxelib.json .
-rm -rf haxe-formatter
-
 for folder in data/*; do (cd $folder; git checkout -- .; git pull); done
 
 for folder in versions/*; do (cd $folder; ./prepare.sh); done
