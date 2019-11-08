fa6ebc77801c1c5cc0bb3822143dcc18a13593a2
AlexHaxe <Alexander.Blum@gmail.com>
2019-11-08 02:19:29 +0100

    reactivate PHP for Haxe 3 runs
    copied index.html from gh-pages branch

--- versions/Haxe-3.4.7/run.sh
+++ versions/Haxe-3.4.7/run.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
-
-mv out/php out/phpOff # currently PHP7 formatter built with Haxe 3 "hangs" somewhere in Lime sources - needs investigation
-
 ./benchmark.sh -s data/haxe/std -s data/openfl/src -s data/lime/src | tee results.csv
 haxe buildConvertCsv.hxml
