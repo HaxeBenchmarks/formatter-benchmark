10f83c5c5074659f7d3588eb257811ce48d8756a
AlexHaxe <Alexander.Blum@gmail.com>
2019-11-08 02:43:53 +0100

    added correct paths

--- cronjob.sh
+++ cronjob.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+cd "$( dirname "${BASH_SOURCE[0]}" )"
+
 ./prepare.sh
 ./build.sh
 ./run.sh
--- versions/Haxe-nightly/run.sh
+++ versions/Haxe-nightly/run.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+cd "$( dirname "${BASH_SOURCE[0]}" )"
+
 readonly VER=`haxe -version`
 echo "Running Haxe $VER benchmark"
 ./benchmark.sh -s data/haxe/std -s data/openfl/src -s data/lime/src | tee results.csv
