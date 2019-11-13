62cfc990026b994b411d3a155842b95c99e86ba2
AlexHaxe <Alexander.Blum@gmail.com>
2019-11-13 20:05:18 +0100

    fixed benchmark script

--- benchmarkNoIO.sh
+++ benchmarkNoIO.sh
@@ -2,35 +2,17 @@
 
 source benchmark.env
 
-readonly DATA=$*
-
-function resetGit {
-    for i in $( ls data ) ; do (cd data/$i; git checkout -- .); done;
-}
-
-resetGit
-./out/cpp/NoIOMain $DATA
-resetGit
-node out/formatter.js $DATA
-resetGit
-node out/formatter.es6.js $DATA
-resetGit
-java -jar out/java/NoIOMain.jar $DATA
-resetGit
-java -jar out/jvm/NoIOMain.jar $DATA
-resetGit
-hl out/formatter.hl $DATA
-resetGit
-./out/hl/formatterCLI $DATA
-resetGit
-mono out/cs/bin/NoIOMain.exe $DATA
-resetGit
-haxe build/common.hxml --run NoIOMain $DATA
-resetGit
-php out/php/index.php $DATA
-resetGit
-neko out/formatter.n $DATA
-resetGit
-python3 out/formatter.py $DATA
-# resetGit
-# lua out/formatter.lua $DATA
+./out/cpp/NoIOMain
+node out/formatter.js
+node out/formatter.es6.js
+java -jar out/java/NoIOMain.jar
+java -jar out/jvm/NoIOMain.jar
+hl out/formatter.hl
+./out/hl/formatterCLI
+mono out/cs/bin/NoIOMain.exe
+haxe build/common.hxml --run NoIOMain
+php out/php/index.php
+neko out/formatter.n
+python3 out/formatter.py
+# 
+# lua out/formatter.lua
